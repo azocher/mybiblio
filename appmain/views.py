7e98c47b85b34908dba944b217d7b4640134a7c2
Anna Zocher <annazocher@Annas-MacBook-Air.local>
2017-06-04 13:40:26 -0700

    commit before data work

--- appmain/views.py
+++ appmain/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # MAH VIEWS ALL DEM VIEWS VIEWS FROM THE SIX
 
 def index(request):
-
+	
+	number = 6
+	return render(request, 'index.html', {'number' : number, })
 	# main view
-	return render(request, 'index.html')
+	#return render(request, 'index.html')

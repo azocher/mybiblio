6b671a2ebb5153a5d829ed8a5f2d4834fc70ec09
Anna Zocher <annazocher@Annas-MacBook-Air.local>
2017-06-06 18:44:29 -0700

    added password reset options / nav / indiviual pages to follow through on action

--- mybiblio/urls.py
+++ mybiblio/urls.py
@@ -4,6 +4,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from appmain import views
+from django.contrib.auth.views import (
+	password_reset,
+	password_reset_done,
+	password_reset_confirm,
+	password_reset_complete
+)
 	
 urlpatterns = [
 	url(r'^$', views.index, name='home'),
@@ -11,6 +17,10 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^books/(?P<slug>[-\w]+)/$', views.book_detail, name='book_detail'),
     url(r'^books/(?P<slug>[-\w]+)/edit/$', views.edit_book, name='edit_book'),
+    url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+    url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name="password_reset_complete"),
     url(r'^accounts/', include('registration.backends.simple.urls')), 
     url(r'^admin/', include(admin.site.urls)), 
 ]
